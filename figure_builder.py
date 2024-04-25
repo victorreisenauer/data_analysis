a58d4a6be02a3dc90328ff06a9d84a5322a69736
victorreisenauer <victorreisenauer99@gmail.com>
2024-04-25 19:34:38 +0200

    Add showline feature to pseudoColorPlots

--- figure_builder.py
+++ figure_builder.py
@@ -32,13 +32,19 @@ class FigureBuilder:
         plt.tight_layout()
         plt.show()
 
-    def pseudoColorPlot(self, dataset :Dataset):
+    def pseudoColorPlot(self, dataset :Dataset, showline=False):
 
         plt.figure(figsize=(8, 6))
     
         x = dataset.give_sample_wavelengths()
         y = dataset.give_sample_numbers()
         z = dataset.give_sample_intensities()
+
+        if showline:
+            first_sample = dataset.give_first_sample()
+            max_index = np.argmax(first_sample[2])
+            max_wavelength = first_sample[1][max_index]
+            plt.axvline(x=max_wavelength, color='r', linestyle='--')
 
         im = plt.pcolormesh(x, y, z)
         plt.colorbar(im)
@@ -47,7 +53,7 @@ class FigureBuilder:
         plt.ylabel('Y')
         plt.show()
 
-    def compare_pseudoColorPlots(self, dataset1 :Dataset, dataset2 :Dataset):
+    def compare_pseudoColorPlots(self, dataset1 :Dataset, dataset2 :Dataset, showline=False):
         fig, axes = plt.subplots(1, 2, figsize=(16, 6))  # Create a figure with two subplots
 
         # Plot data from dataset1 (subplot 1)
@@ -60,6 +66,9 @@ class FigureBuilder:
         axes[0].set_xlabel('X')
         axes[0].set_ylabel('Y')
 
+        if showline:
+            self._add_showline(axes[0], dataset1)
+
         # Plot data from dataset2 (subplot 2)
         axes[1].set_title('Data from Dataset 2')  # Set title for subplot 2
         x2 = dataset2.give_sample_wavelengths()
@@ -70,9 +79,18 @@ class FigureBuilder:
         axes[1].set_xlabel('X')
         axes[1].set_ylabel('Y')
 
+        if showline:
+            self._add_showline(axes[1], dataset2)
+
         # Adjust layout and display the plots
         plt.tight_layout()
         plt.show()
+
+    def _add_showline(self, ax, dataset: Dataset):
+        first_sample = dataset.give_first_sample()
+        max_index = np.argmax(first_sample[2])
+        max_wavelength = first_sample[1][max_index]
+        ax.axvline(x=max_wavelength, color='r', linestyle='--')
     
     def quickPlot(self, dataset :Dataset):
         plt.figure(figsize=(8, 6))
--- main.py
+++ main.py
@@ -10,5 +10,7 @@ fb = FigureBuilder()
 ds1 = dm.build_dataset_from("240411_cont_staticPL_sample_1")
 ds2 = dm.build_dataset_from("240412_cont_staticPL_sample 2")
 
-fb.compare_pseudoColorPlots(ds1, ds2)
+
+
+fb.compare_pseudoColorPlots(ds1, ds2, showline=True)
 
